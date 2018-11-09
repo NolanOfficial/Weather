c1a4300065a2083297be037ec28614f75809f9be
Nolan Fuchs <nolan1235@gmail.com>
2018-11-09 13:24:13 -0700

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Nolan Fuchs on 11/9/18.
+//  Copyright © 2018 Nolan Fuchs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
